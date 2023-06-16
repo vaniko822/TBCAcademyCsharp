72d3222a09bd32512e800d54ecb4a68c832cecaa
Vaniko <83824294+vaniko822@users.noreply.github.com>
2023-06-16 16:10:39 +0400

    recovered Assignment05,06

---
+++ Assignment 05/Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Sheiyvanet masivis zoma: ");
+int arraySize = int.Parse(Console.ReadLine());
+
+int[] arrayNumbers = new int[arraySize];
+
+Console.WriteLine("Sheiyvanet masivis elementebi: ");
+for (int i = 0; i < arraySize; i++)
+{
+    Console.WriteLine($"[{i}]");
+    int arrayNumber = int.Parse(Console.ReadLine());
+    arrayNumbers[i] = arrayNumber;
+}
+
+Console.WriteLine("Sheiyvanet index: ");
+int index = int.Parse(Console.ReadLine());
+
+ElementisCifrebisJamisGamotvla(arrayNumbers, index);
+
+void ElementisCifrebisJamisGamotvla(int[] array, int index)
+{
+    int sum = 0;
+    int number = arrayNumbers[index];
+
+    while (number != 0)
+    {
+        int digit = number % 10;
+        sum += digit;
+        number /= 10;
+    }
+
+    Console.WriteLine($"Elementis cifris jami: {sum}");
+}
+
+
+/* es meore variantia ubralod uaryofiti tu sheiyvana imas ver itvlis.
+ *  mindoda gameketebina stringshi gadameyvana da mere charebit gadametarebina
+ *  cikli da gadameyvana es charebi intshi isev da dametvala.
+ */
+//void ElemetisCifrebisJamisGamotvla2(int[] array, int index)
+//{
+//    if(index >= 0 && index < array.Length)
+//    {
+//        int number = arrayNumbers[index];
+//        string numberToString = Convert.ToString(number);
+//        int sum = 0;
+
+//        for(int i = 0; i < numberToString.Length; i++)
+//        {
+//            char c = numberToString[i];
+//            string s = c.ToString();
+//            int numberToInt = int.Parse(s);
+//            sum += numberToInt;
+//        }
+//        Console.WriteLine($"Elementis cifris jami: {sum}");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Indexi naklebia 0ze an metia masivis zomaze");
+//    }
+//}
---
+++ Assignment 06/Task1/Program.cs
@@ -0,0 +1,77 @@
+﻿using LibrarySystem;
+
+Book book1 = new Book()
+{
+    Title = "Treasure Island",
+    Author = "Robert Louis Stevenson",
+    Year = 1883,
+    Genre = "Adventure",
+    Price = 50
+};
+
+Book book2 = new Book()
+{
+    Title = "The Three Musketeers",
+    Author = "Alexandre Dumas",
+    Year = 1844,
+    Genre = "Adventure",
+    Price = 70
+};
+
+Book book3 = new Book()
+{
+    Title = "The Lord of the Rings",
+    Author = "J.R.R. Tolkien",
+    Year = 1954,
+    Genre = "Fantasy",
+    Price = 100
+};
+
+Library lib = new Library();
+
+/* Arrays zomis gansazgvra */
+lib.LibraryCapacity = 10;
+
+/* library-is klasis wignebis masivshi Elementebis damateba */
+lib.AddBook(book1);
+lib.AddBook(book2);
+lib.AddBook(book3);
+
+/* Indexserit wignebis masivshi wignis amogeba */
+var book = lib[1];
+Console.WriteLine(
+    $"Title - {book.Title}" + "\n" +
+    $"Author - {book.Author}" + "\n" +
+    $"Year - {book.Year}" + "\n" +
+    $"Genre - {book.Genre}" + "\n" +
+    $"Price - {book.Price}"
+    );
+
+/* Library klasshi arsebuli wignebis raodenoba */
+var count = lib.Count;
+Console.WriteLine($"Wignebis Raodenoba - {count} Wigni");
+
+/* Library klasshi arsebuli wignebis masivshi wignis modzebna sataurit */
+var findbook = lib.FindBook("");
+if (findbook != null)
+{
+    Console.WriteLine(
+    $"Title - {findbook.Title}" + "\n" +
+    $"Author - {findbook.Author}" + "\n" +
+    $"Year - {findbook.Year}" + "\n" +
+    $"Genre - {findbook.Genre}" + "\n" +
+    $"Price - {findbook.Price}"
+    );
+}
+
+/* Library klasshi arsebuli wignebis masivshi edzebs 
+ * chven gadacemu wigns da shlis masividan */
+var removeBook = lib.RemoveBook(book1);
+if(removeBook == true)
+{
+    Console.WriteLine("Wigni Waishala!");
+}
+else
+{
+    Console.WriteLine("Es Wigni Ar Aris Bibliotekashi!");
+}
