6cce87a4fa0c31b3e75b50311b96fa7006342cc7
Vano Kukuchanashvili <vkukuchanashvili@tbcbank.com.ge>
2023-06-26 22:18:21 +0400

    Assignment10

---
+++ Assignment 10/Task1/Program.cs
@@ -0,0 +1,69 @@
+﻿using Task1;
+
+int[,] matrixConsole = new int[2, 2];
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        Console.Write($"Enter the element [{i}, {j}]: ");
+        string number = Console.ReadLine();
+        matrixConsole[i, j] = int.Parse(number);
+    }
+}
+
+Matrix matrix = new Matrix(matrixConsole);
+
+Console.WriteLine("Matrix 1: ");
+matrix.PrintMatrix();
+
+int[,] matrixConsole2 = new int[2, 2];
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        Console.Write($"Enter the element [{i}, {j}]: ");
+        string number = Console.ReadLine();
+        matrixConsole2[i, j] = int.Parse(number);
+    }
+}
+
+// Implicit conversion from int[,] to Matrix
+Matrix matrix2 = matrixConsole2;
+Console.WriteLine("Matrix 2: ");
+matrix2.PrintMatrix();
+
+// Equals()
+var equals = matrix.Equals(matrix2);
+Console.WriteLine($"Matrix1 is equals Matrix2? - {equals}");
+
+// +
+var sum = matrix + matrix2;
+
+Console.WriteLine("Sum Matrix: ");
+sum.PrintMatrix();
+
+// -
+var subtraction = matrix - matrix2;
+
+Console.WriteLine("Subtraction Matrix: ");
+subtraction.PrintMatrix();
+
+// *
+var multiplied = matrix * matrix2;
+
+Console.WriteLine("Multiplied Matrix: ");
+multiplied.PrintMatrix();
+
+// (-)
+var inversion = -matrix;
+Console.WriteLine("Inversion Matrix: ");
+inversion.PrintMatrix();
+
+// ToString()
+Console.WriteLine("Matrix ToString():");
+Console.WriteLine(matrix.ToString());
+
+
+Console.ReadKey();
---
+++ Assignment 10/Task2/Triangle.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Triangle
+    {
+        private double _s1 { get; set; }
+        private double _s2 { get; set; }
+        private double _s3 { get; set; }
+
+        public Triangle(double side_1, double side_2, double side_3)
+        {
+            this._s1 = side_1;
+            this._s2 = side_2;
+            this._s3 = side_3;
+            checkEqualityTriangle();
+        }
+
+        public double S1 { get { return _s1;} }
+        public double S2 { get { return _s2;} }
+        public double S3 { get { return _s3;} }
+
+        public double Parameter()
+        {
+            double parameter = _s1 + _s2 + _s3;
+            return parameter;
+        }
+
+        public double Area()
+        {
+            double semiPerimeter = (_s1 + _s2 + _s3) / 2;
+            double area = Math.Sqrt(semiPerimeter * (semiPerimeter - _s1) * (semiPerimeter - _s2) * (semiPerimeter - _s3));
+
+            return area;
+        }
+
+        public static bool operator ==(Triangle a, Triangle b)
+        {
+            return a._s1 == b._s1 && a._s2 == b._s2 && a._s3 == b._s3;
+        }
+
+        public static bool operator !=(Triangle a, Triangle b)
+        {
+            return a._s1 != b._s1 || a._s2 != b._s2 || a._s3 != b._s3;
+        }
+
+        public static bool operator <(Triangle a, Triangle b)
+        {
+            return a.Area() < b.Area();
+        }
+
+        public static bool operator >(Triangle a, Triangle b)
+        {
+            return a.Area() > b.Area();
+        }
+
+        public static Triangle operator +(Triangle a, Triangle b)
+        {
+            long totalArea = (long)(a.Area() + b.Area());
+            double side = Math.Sqrt((double)totalArea);
+
+            Triangle result = new Triangle(side, side, Math.Sqrt(2) * side);
+            return result;
+        }
+
+        public bool checkEqualityTriangle()
+        {
+            if(_s1 + _s2 <= _s3 || _s1 + _s3 <= _s2 || _s2 + _s3 <= _s1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static explicit operator Triangle(double side)
+        {
+            Triangle equilateralTriangle = new Triangle(side, side, side);
+            return equilateralTriangle;
+        }
+
+    }
+}
