70b1e6b3106630b3b0e061686bf2aa88391ef3bd
Vano Kukuchanashvili <vkukuchanashvili@tbcbank.com.ge>
2023-07-03 13:06:26 +0400

    Assignment13

---
+++ Assignment 13/Task1/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Book
+    {
+        private int _bookNumber { get; set; }
+        private string _title { get; set; }
+        private string _author { get; set; }
+
+        public Book(int bookNumber, string title, string author) 
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException("Title cannot be empty.");
+            }
+
+            if (string.IsNullOrEmpty(author))
+            {
+                throw new ArgumentNullException("Author cannot be empty.");
+            }
+
+            this._bookNumber = bookNumber;
+            this._title = title;
+            this._author = author;
+        }
+
+        public int BookNumber { get { return _bookNumber; } set { _bookNumber = value; } } 
+        public string Title { get { return _title; } set { _title = value; } }
+        public string Author { get { return _author; } set { _author = value; } }
+    }
+}
