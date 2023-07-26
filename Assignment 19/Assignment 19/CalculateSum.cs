1451007cd8fe7871c687782ae3ba54f8f6da4eda
Vano Kukuchanashvili <vkukuchanashvili@tbcbank.com.ge>
2023-07-26 12:40:55 +0400

    Assignment19

---
+++ Assignment 19/Assignment 19/CalculateSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_19
+{
+    public class CalculateSum
+    {
+        public static long CalculateSequentialSum(int[] array)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            long sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"Sequential calculation took {stopwatch.ElapsedMilliseconds} ms");
+            return sum;
+        }
+        public static void CalculatePartialSum(int start, int end, int threadIndex, int[] array, int[] partialSums)
+        {
+            int partialSum = 0;
+            for (int i = start; i < end; i++)
+            {
+                partialSum += array[i];
+            }
+
+            partialSums[threadIndex-1] = partialSum;
+
+        }
+    }
+}
